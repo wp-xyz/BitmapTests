177f245e884f610a6915f05b43068617fe8b638f
wp_xyz <wp_xxyyzzz@gmx.net>
2025-08-02 13:08:55 +0200

    Provide 32bpp images

--- image_generator/main.pas
+++ image_generator/main.pas
@@ -13,11 +13,12 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
     Image1: TImage;
     Image2: TImage;
     Image3: TImage;
     Image4: TImage;
-    procedure Button1Click(Sender: TObject);
+    procedure RunButtonClick(Sender: TObject);
   private
 
   public
@@ -33,36 +34,46 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TForm1.RunButtonClick(Sender: TObject);
 const
   DIR = '../expected_images/';
   BACKGROUND_COLOR = clWhite;
 var
   bmp: TCustomBitmap;
+  bppStr: String;
 begin
   ForceDirectories(DIR);
   bmp := TBitmap.Create;
   try
     bmp.SetSize(32, 32);
+    if Sender = Button1 then
+    begin
+      bppStr := ''
+    end else
+    begin
+      bppStr := '_32bpp';
+      bmp.PixelFormat := pf32Bit;
+    end;
+
     bmp.Canvas.Brush.Color := clRed;   // red background
     bmp.Canvas.FillRect(0, 0, bmp.Width, bmp.Height);
-    bmp.SaveToFile(DIR+'expected_bkgr.bmp');
+    bmp.SaveToFile(DIR+'expected_bkgr' + bppStr + '.bmp');
     Image1.Picture.Assign(bmp);
 
     bmp.Canvas.Brush.Color := clBlue;  // blue inserted rectangle
     bmp.Canvas.FillRect(8, 8, 24, 24);
-    bmp.SaveToFile(DIR+'expected_blueRect.bmp');
+    bmp.SaveToFile(DIR+'expected_redBorder_blueRect' + bppStr + '.bmp');
     Image2.Picture.Assign(bmp);
 
     bmp.canvas.Brush.Color := BACKGROUND_COLOR;  // color-transparent inserted rectangle
     bmp.Canvas.FillRect(8, 8, 24, 24);
-    bmp.SaveToFile(DIR+'expected_blue_transparent.bmp');
+    bmp.SaveToFile(DIR+'expected_blue_transparent' + bppStr + '.bmp');
     Image3.Picture.Assign(bmp);
 
     bmp.Canvas.FillRect(0, 0, 32, 32);
     bmp.Canvas.Brush.Color := clBlue;
     bmp.Canvas.FillRect(8, 8, 24, 24);
-    bmp.SaveToFile(DIR+'expected_red_transparent.bmp');
+    bmp.SaveToFile(DIR+'expected_red_transparent' + bppStr + '.bmp');
     Image4.Picture.Assign(bmp);
   finally
     bmp.Free;
